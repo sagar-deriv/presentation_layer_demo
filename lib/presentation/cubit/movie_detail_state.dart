7190aae80ad7da72cd7b0e155d6fb4cc4207ad19
Sagar Deriv <sagar@deriv.com>
2024-02-21 21:20:01 +0400

    hehe

--- lib/presentation/cubit/movie_detail_state.dart
+++ lib/presentation/cubit/movie_detail_state.dart
@@ -7,29 +7,3 @@ class MovieDetailState with _$MovieDetailState {
   const factory MovieDetailState.loaded({required Movie movie}) = _Loaded;
   const factory MovieDetailState.error({required String errorMessage}) = _Error;
 }
-
-
-
-// abstract class MovieDetailState {}
-
-// class MovieDetailInitialState extends MovieDetailState {
-//   MovieDetailInitialState();
-// }
-
-// class MovieDetailLoadingState extends MovieDetailState {
-//   MovieDetailLoadingState();
-// }
-
-// class MovieDetailLoadedState extends MovieDetailState {
-//   final Movie movie;
-//   MovieDetailLoadedState({
-//     required this.movie,
-//   });
-// }
-
-// class MovieDetailErrorState extends MovieDetailState {
-//   final String errorMessage;
-//   MovieDetailErrorState({
-//     required this.errorMessage,
-//   });
-// }
